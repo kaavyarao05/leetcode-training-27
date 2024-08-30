bbfafdcf32899106a08fcd28ddd75e729d96aa29
kaavya <kaavyarao05@gmail.com>
2024-08-30 16:50:05 +0530

    Week 1 soln

--- week-01/solution.cpp
+++ week-01/solution.cpp
@@ -5,6 +5,16 @@ int main(){
     std::cin>>a>>n;
 
     for (int i=0;i<=n;i++){
-        
+        if((i%3)==0){
+            std::cout<<"Foo"<<"\n";
+        }
+        else{
+            if((i&1)==0){
+                std::cout<<"Bar"<<"\n";
+            }
+            else{
+                std::cout<<"Baz"<<"\n";
+            }
+        }
     }
 }
